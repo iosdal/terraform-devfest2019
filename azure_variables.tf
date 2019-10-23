4c77717a58033a1b733d7ab47aadb13e425b4491
Ivan <idallaserra@fbk.eu>
2019-10-23 14:49:32 +0200

    removed unused variables

--- azure_variables.tf
+++ azure_variables.tf
@@ -7,18 +7,23 @@ variable "tenant_id" {
 variable "subscription_id" {
 }
 variable "location" {
+    default = "westeurope"
 }
 variable "cluster_name" {
+    default = "myk8s"
 }
 variable "agent_count" {
+    default = "1"
 }
 variable "dns_prefix" {
+    default = "devfestdemo"
 }
 variable "terraform_resource_group" {
+    default = "devfest"
 }
 variable "terraform_env" {
+    default = "Devfest Demo"
 }
 variable "terraform_env_owner" {
+    default = "ivandal77@gmail.com"
 }
-variable "analytics_name" {
-} 
